196118a86bad0936230b83f6024be1c6b7719072
Lasse <lasseschaar@gmail.com>
2023-03-13 20:16:08 +0100

    hab mal kleine tweaks gemacht

--- Map.py
+++ Map.py
@@ -5,7 +5,7 @@ G = nx.MultiDiGraph()
 H = nx.MultiDiGraph()
 G_speed_lim = 13.8889
 H_speed_lim = 13.8889
-ort = {1:(-100, 0), 2:(0, 0), 3:(0, -100), 4:(0, 100)}
+ort = {1:(-160, 0), 2:(0, 0), 3:(0, -100), 4:(0, 100)}
 strecken_G = [(1, 2, (distance.euclidean(ort[1], ort[2])/G_speed_lim))]
 strecken_H = [(3, 2, (distance.euclidean(ort[3], ort[2])/H_speed_lim)), (2, 4, (distance.euclidean(ort[2], ort[4])/H_speed_lim))]
 G.add_weighted_edges_from(strecken_G)
