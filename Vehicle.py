3133f1506c86527f234e366cf4391ec07a7ec63e
Lasse <lasseschaar@gmail.com>
2023-03-13 22:17:10 +0100

    hab mal kleine tweaks gemacht

--- Vehicle.py
+++ Vehicle.py
@@ -47,7 +47,7 @@ class Vehicle:
                     continue
                 else:
                     for car in self.sim.roads[i].vehicles[self.sim.roads[i].edges.index(t)]:
-                        if self.sim.roads[i].length[self.sim.roads[i].edges.index(t)] - car.x <= self.sim.roads[i].speed_lim * 2.5:
+                        if self.sim.roads[i].length[self.sim.roads[i].edges.index(t)] - car.x <= self.sim.roads[i].speed_lim * 2.7:
                             self.a = -self.b_max * self.v / self.v_max[self.current_edge_index] - 1.337
 
 
--- main.py
+++ main.py
@@ -11,23 +11,18 @@ from Map import *
 
 
 sim = Simulation()
-sim.create_road(ort, strecken_G, G_speed_lim, 2)
-sim.create_road(ort, strecken_H, H_speed_lim, 1)
-#sim.create_road(ort, strecken_K, K_speed_lim)
+sim.create_road(ort, strecken_G, G_speed_lim, 0)
+sim.create_road(ort, strecken_H, H_speed_lim, 2)
+sim.create_road(ort, strecken_K, K_speed_lim, 1)
 sim.roads[0].vehicles[0].append(
-  Vehicle(J, sim, {"path": strecken_G + strecken_H[1:], "v_max": [G_speed_lim, H_speed_lim]})
- )
-sim.roads[1].vehicles[0].append(
- Vehicle(J, sim, {"path": strecken_H, "v_max": [H_speed_lim, H_speed_lim]})
+  Vehicle(I, sim, {"path": strecken_G + strecken_H[1:], "v_max": [G_speed_lim, H_speed_lim]})
+)
+sim.roads[2].vehicles[0].append(
+  Vehicle(I, sim, {"path": strecken_K + strecken_H[1:], "v_max": [G_speed_lim, H_speed_lim]})
 )
 sim.roads[1].vehicles[0].append(
- Vehicle(J, sim, {"path": strecken_H, "v_max": [H_speed_lim, H_speed_lim]})
+ Vehicle(I, sim, {"path": strecken_H, "v_max": [H_speed_lim, H_speed_lim]})
 )
-sim.roads[1].vehicles[0].append(
- Vehicle(J, sim, {"path": strecken_H, "v_max": [H_speed_lim, H_speed_lim]})
-)
-sim.roads[1].vehicles[0].append(
- Vehicle(J, sim, {"path": strecken_H, "v_max": [H_speed_lim, H_speed_lim]})
-)
+
 win = Window(sim)
 win.run(steps_per_update = 3)
