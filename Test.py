32459e96b97c8e37927ba5a3bab3420a76f65990
LLLLLLLLLLLLLLLLLLL <lasseschaar@gmail.com>
2023-04-15 17:33:11 +0200

    Mit comments

--- Test.py
+++ Test.py
@@ -1,6 +1,17 @@
-c = [1, 1, 1, 1, 1, 1, 1]
-for i in range(len(c)):
-    print(i)
+import numpy as np
+
+c = [(1, 1, 1, 1), (2, 1, 1, 3), (1, 6, 1, 6)]
+c_a = np.array(c)
+
+zwei = np.where(np.all(c_a == np.array((1, 6, 1, 6)), axis = 1))
+for i in c_a:
+    print(tuple(i))
+f = c_a[2]
+b = c_a.tolist()
+print(c_a)
+print(b)
+print(zwei[0][0])
+print(f)
 
 
 
